20715425236b1e7ac5dc3896803bbad255fffc46
Salomao <salomao.machado.75@gmail.com>
2023-06-29 21:42:06 -0300

    <Tarefa> Criação de classe adicionais; Criação de classes cadastro e busca po nome; Classe cadastro cria um array para guardar as informações dos alunos. Classe busca varre o array para encontrar os alunos com o nome

---
+++ src/BuscaAlunoPorNome.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuscaAlunoPorNome {
+    public void buscaAlunoPorNome(String nome, List<Aluno> listaAlunos){
+        List<Aluno> alunosEncontrados = new ArrayList<>();
+
+        for(Aluno aluno: listaAlunos){
+            if(aluno.getNome().equalsIgnoreCase(nome)){
+                alunosEncontrados.add(aluno);
+            }
+        }
+
+        if(alunosEncontrados.isEmpty()){
+            System.out.println("Desculpe nenhum aluno com o nome "+ nome + " foi encontrado");
+        }else{
+            System.out.println("Alunos cadastrados: ");
+            for(Aluno aluno : alunosEncontrados){
+                System.out.println("Nome: " + aluno.getNome());
+                System.out.println("Idade: " + aluno.getIdade());
+                System.out.println("Curso: " + aluno.getCurso());
+                System.out.println("--------------------------");
+            }
+        }
+    }
+}
---
+++ src/CadastroAlunos.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CadastroAlunos {
+
+    private List<Aluno> listaAlunos;
+
+    public CadastroAlunos(){
+        listaAlunos = new ArrayList<>();
+    }
+
+    public void cadastrarAluno(String nome, int idade, String curso){
+        Aluno aluno = new Aluno(nome, idade, curso);
+        listaAlunos.add(aluno);
+    }
+
+    public void listarAlunos(){
+        if(listaAlunos.isEmpty()){
+            System.out.println("Não há alunos cadastrados no momento");
+        }else{
+            System.out.println("Lista de alunos");
+            for(Aluno aluno: listaAlunos){
+                System.out.println("Nome: " + aluno.getNome());
+                System.out.println("Idade: " + aluno.getIdade());
+                System.out.println("Curso: " + aluno.getCurso());
+                System.out.println("--------------------------");
+            }
+        }
+    }
+    public List<Aluno> getListaAlunos() {
+        return listaAlunos;
+    }
+
+    public void setListaAlunos(List<Aluno> listaAlunos) {
+        this.listaAlunos = listaAlunos;
+    }
+
+}
