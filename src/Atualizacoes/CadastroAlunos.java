f680baa6b0a67b65d7d03f39098bed0598eb124d
Salomao <salomao.machado.75@gmail.com>
2023-07-01 21:51:15 -0300

    <feat> importação e metodos: Criando metodos para salvar os dados em um arquivo js que servirá como banco de dados fictício; importação de bibliotecas para utilizar os metodos GSON.

--- src/Atualizacoes/CadastroAlunos.java
+++ src/Atualizacoes/CadastroAlunos.java
@@ -1,21 +1,30 @@
 package Atualizacoes;
-
 import Bases.Aluno;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class CadastroAlunos {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
-    private List<Aluno> listaAlunos;
+
+public class CadastroAlunos {
+    private final String bancoDeDados = "dbAluno.json";
+
+    private final List<Aluno> listaAlunos;
 
     public CadastroAlunos(){
+
         listaAlunos = new ArrayList<>();
+        carregarDoArquivo();
     }
 
     public void cadastrarAluno(String nome, int idade, String curso){
         Aluno aluno = new Aluno(nome, idade, curso);
         listaAlunos.add(aluno);
+        salvarNoBD();
     }
 
     public void listarAlunos(){
@@ -31,12 +40,36 @@ public class CadastroAlunos {
             }
         }
     }
-    public List<Aluno> getListaAlunos() {
-        return listaAlunos;
+
+    public void salvarNoBD(){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try(FileWriter writer = new FileWriter(bancoDeDados)){
+            gson.toJson(listaAlunos, writer);
+            System.out.println("Aluno cadastrado no banco de dados");
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar os alunos no arquivo: " + e.getMessage());
+        }
     }
 
-    public void setListaAlunos(List<Aluno> listaAlunos) {
-        this.listaAlunos = listaAlunos;
+    public void carregarDoArquivo(){
+        Gson gson = new Gson();
+        try{
+            File file = new File(bancoDeDados);
+            if(file.exists()) {
+                FileReader reader = new FileReader(bancoDeDados);
+                Aluno[] alunosArray = gson.fromJson(reader, Aluno[].class);
+                listaAlunos.addAll(Arrays.asList(alunosArray));
+                reader.close();
+                System.out.println("Dados dos alunos carregados do arquivo.");
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar os dados dos alunos do arquivo: " + e.getMessage());
+        }
+    }
+
+    public List<Aluno> getListaAlunos() {
+        return listaAlunos;
     }
 
 }
